1db665d5f3325ad2e49e09f86f419e0af2844c71
Akinyemi Olalekan <69955888+primus02@users.noreply.github.com>
2021-01-11 16:46:25 +0000

    Update apporderdetails.js

--- scripts/apporderdetails.js
+++ scripts/apporderdetails.js
@@ -21,7 +21,7 @@ signOutButton.addEventListener("click", ()=>{
     window.location.href = "index.html";
 });
 
-const url = "http://localhost:3000";
+const url = "https://sendit.herokuapp.com/";
 
 const token = localStorage.getItem("token");
 
